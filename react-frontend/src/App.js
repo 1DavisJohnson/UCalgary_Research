bcb17c4033feecdbdb5c355eba097f2a07e294c5
Sultan Arafat <sultanarafat10@gmail.com>
2022-01-27 15:11:36 -0700

    added the landing page

--- react-frontend/src/App.js
+++ react-frontend/src/App.js
@@ -1,9 +1,18 @@
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
+import Info from "./components/Information/Info";
 
+// Style sheet
+<link
+  rel="stylesheet"
+  href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+></link>;
 
 function App() {
   return (
     <div className="App">
-      University of Calgary - Research
+      <Navbar />
+      <Info />
     </div>
   );
 }
